7cbdf999acc2e8f352d07b572c35115648ac5758
gomachan3232 <75553558+gomachan3232@users.noreply.github.com>
2021-01-30 13:35:34 +0900

    商品出品機能の実装

---
+++ app/models/item.rb
@@ -0,0 +1,25 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :send_day
+  belongs_to :user
+  has_one :buy
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :info
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :shipping_fee_id
+      validates :prefecture_id
+      validates :send_day_id
+    end
+    validates :price, inclusion: { in: 300..9_999_999 }
+  end
+end
