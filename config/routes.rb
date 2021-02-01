116a640ac885ef78719d26312e64f8900c9e69b2
gomachan3232 <75553558+gomachan3232@users.noreply.github.com>
2021-02-01 20:11:36 +0900

    商品詳細表示機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:new, :create]
+  resources :items, only: [:new, :create, :show]
 
-  
+  get "/item/id", to: "items#show"
+
 end
