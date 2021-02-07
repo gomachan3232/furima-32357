e17d7b4524bb0d69da783df976ebb3ab81be7f5a
gomachan3232 <75553558+gomachan3232@users.noreply.github.com>
2021-02-07 10:18:40 +0900

    商品購入ページの作成

---
+++ app/models/address.rb
@@ -0,0 +1,14 @@
+class Address < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :buy
+
+  with_options presence: true do
+    validates :post_code
+    validates :prefecture_id , numericality: { other_than: 1 }
+    validates :city 
+    validates :address
+    validates :phone_number
+  end
+  
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :send_day
   belongs_to :user, optional: true
-  # has_one :buy
+  has_one :buy
   has_one_attached :image
 
   with_options presence: true do
